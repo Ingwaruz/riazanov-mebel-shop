f4ac9e3ecd68214dfeb2d6c9dc4fc1defbc78a59
Игорь <tawer0228@gmail.com>
2024-08-21 22:33:18 +1000

    add routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
+const cors = require('cors')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
