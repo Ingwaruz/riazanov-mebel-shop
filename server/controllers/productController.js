14e8775be197c118a160c11fd8506b06c61e4c39
Игорь <tawer0228@gmail.com>
2024-08-22 23:11:42 +1000

    add info parsing

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,17 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Product} = require('../models/models')
+const {Product, ProductInfo} = require('../models/models')
 const ApiError = require('../error/apiError')
 
 class productController {
     async create(req, res, next) {
         try {
-            const {name, price, factoryId, typeId, info} = req.body
+            let {name, price, factoryId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             const product = await Product.create({name, price, factoryId, typeId, img: fileName})
+
+            if(info) {
+                info = JSON.parse(info)
+                info.array.forEach(element => {
+                    ProductInfo.create({
+                        title: element.title,
+                        describtion: element.describtion,
+                        productId: product.id
+                    })
+                });
+            }
 
             return res.json(product)
         } catch (e) {
