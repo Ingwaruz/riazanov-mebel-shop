d1f027135b70f52b939992d30c0d58c61571d091
Игорь <tawer0228@gmail.com>
2024-08-22 14:05:32 +1000

    add create, getall functions for types

--- server/routes/factoryRouter.js
+++ server/routes/factoryRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
+const factoryController = require('../controllers/factoryController')
 
-router.post('/',)
-router.get('/',)
+router.post('/', factoryController.create)
+router.get('/', factoryController.getAll)
 router.delete('/',)
 
 module.exports = router
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,7 +1,8 @@
 const Router = require('express')
 const router = new Router()
+const typeController = require('../controllers/typeController')
 
-router.post('/',)
-router.post('/',)
+router.post('/', typeController.create)
+router.post('/', typeController.getAll)
 
 module.exports = router
