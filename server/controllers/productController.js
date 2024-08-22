6490fa7d715ebd0b629b443c4dfece8f256b6cb0
Игорь <tawer0228@gmail.com>
2024-08-22 14:48:27 +1000

    add create function for product (with img), install express-fileupload, uuid

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,22 @@
+const uuid = require('uuid')
+const path = require('path')
+const {Product} = require('../models/models')
+const ApiError = require('../error/apiError')
+
 class productController {
-    async create(req, res) {
-        const
+    async create(req, res, next) {
+        try {
+            const {name, price, factoryId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const product = await Product.create({name, price, factoryId, typeId, img: fileName})
+
+            return res.json(product)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 
     async getAll(req, res) {
