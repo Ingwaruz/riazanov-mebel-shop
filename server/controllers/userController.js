70e7a685bc4ad9cccc18692aff7c0d174deb5a27
Игорь <tawer0228@gmail.com>
2024-08-24 00:03:05 +1000

    user login and authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,16 +35,24 @@ class userController {
         }
     }
 
-    async login(req, res) {
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден :('))
+        }
 
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Неверный пароль :('))
+        }
+
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async checkAuth(req, res, next) {
-        const {id} = req.query
-        if (!id) {
-            return next(ApiError.badRequest('Не задан id'))
-        }
-        res.json(id) 
+        res.json({message: "working"})
     }
 }
 
