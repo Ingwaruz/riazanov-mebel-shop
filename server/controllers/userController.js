31043d598da767d8576529614ff2fff1fd4e0865
Игорь <tawer0228@gmail.com>
2024-08-23 20:40:43 +1000

    add user registration with jwt

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,27 @@
 const ApiError = require('../error/apiError');
+const bcript = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
 
 class userController {
     async registartion(req, res) {
-
+        const {email, password, role} = req.body
+        if(!email || !password) {
+            return next(ApiError.badRequest('Неверная почта или пароль'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с такой почтой уже существует!'))
+        }
+        const hashPassword = await bcript.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const Basket = await Basket.create({userId: user.id})
+        const token = jwt.sign(
+            {id: user.id, email, role}, 
+            process.env.SECRET_KEY,
+            {expiresIn: '24h'}
+        )
+        return res.json({token})
     }
 
     async login(req, res) {
