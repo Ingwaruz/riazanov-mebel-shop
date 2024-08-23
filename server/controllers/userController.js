099ca47fb9d497e88c1cc3926b3d2f64c1f223f9
Игорь <tawer0228@gmail.com>
2024-08-23 23:38:09 +1000

    fix user registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,27 +1,38 @@
 const ApiError = require('../error/apiError');
-const bcript = require('bcrypt')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
 class userController {
-    async registartion(req, res) {
-        const {email, password, role} = req.body
-        if(!email || !password) {
-            return next(ApiError.badRequest('Неверная почта или пароль'))
+    async registration(req, res, next) {
+        try {
+            const { email, password, role } = req.body;
+            if (!email || !password) {
+                return next(ApiError.badRequest('Неверная почта или пароль'));
+            }
+
+            const candidate = await User.findOne({ where: { email } });
+            if (candidate) {
+                return next(ApiError.badRequest('Пользователь с такой почтой уже существует!'));
+            }
+
+            const hashPassword = await bcrypt.hash(password, 5);
+            const user = await User.create({ email, role, password: hashPassword });
+            await Basket.create({ userId: user.id });
+
+            const token = generateJwt(user.id, user.email, user.role);
+            return res.json({ token });
+        } catch (error) {
+            return next(ApiError.internal('Ошибка сервера'));
         }
-        const candidate = await User.findOne({where: {email}})
-        if (candidate) {
-            return next(ApiError.badRequest('Пользователь с такой почтой уже существует!'))
-        }
-        const hashPassword = await bcript.hash(password, 5)
-        const user = await User.create({email, role, password: hashPassword})
-        const Basket = await Basket.create({userId: user.id})
-        const token = jwt.sign(
-            {id: user.id, email, role}, 
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
-        return res.json({token})
     }
 
     async login(req, res) {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,7 +2,7 @@ const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
 
-router.post('/registration', userController.registartion)
+router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', userController.checkAuth)
 
