af75a641881f41b4e6c7931f8b361123b7104519
Игорь <tawer0228@gmail.com>
2024-08-24 13:51:35 +1000

    add navbar from react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <BrowserRouter>
+        <Navbar />
         <AppRouter />
     </BrowserRouter>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navbar as BootstrapNavbar, Container, Nav } from 'react-bootstrap';
+import { Context } from "../index";
 
-const Navbar = () => {
+const MyNavbar = () => {
+    const { user } = useContext(Context);
+
     return (
         <div>
-            
+            <BootstrapNavbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <BootstrapNavbar.Brand href="#home">Navbar</BootstrapNavbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </BootstrapNavbar>
         </div>
     );
 };
 
-export default Navbar;
+export default MyNavbar;
