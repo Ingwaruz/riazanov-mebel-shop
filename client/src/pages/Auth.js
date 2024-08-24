f9a023b9ed3b3780022d85abdb85b94528b5ace6
Игорь <tawer0228@gmail.com>
2024-08-24 16:48:14 +1000

    add auth form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
     return (
-        <Container>
-            
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">Авторизация</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш email..."
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите ваш пароль..."
+                    />
+                    <Row className="d-flex justify-content-between pl-3 pr-3">
+                        <div>
+                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink>
+                        </div>
+                        <Button
+                            className="mt-3 align-self-end"
+                            variant={"outline-success"}
+                        >
+                            Войти
+                        </Button>
+                    </Row>
+                </Form>
+            </Card>
         </Container>
     );
 };
