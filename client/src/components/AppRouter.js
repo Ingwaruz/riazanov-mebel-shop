0fb55f8cc9ee0025d837020d1945905bfd1c4779
Игорь <tawer0228@gmail.com>
2024-08-24 13:05:12 +1000

    better auth check

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Routes, Route} from 'react-router-dom'
 import {authRoutes, publicRoutes} from "../routes";
 import Shop from "../pages/Shop";
+import {Context} from "../index";
 const AppRouter = () => {
-    const isAuth = true
+    const {user} = useContext(Context)
+
+    console.log(user)
     return (
         <div>
             <Routes>
-                {isAuth && authRoutes.map(({path, Component}) =>
+                {user.isAuth && authRoutes.map(({path, Component}) =>
                     <Route key={path} path={path} element={<Component/>} exact/>
                 )}
                 {publicRoutes.map(({path, Component}) =>
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserStore from "./store/UserStore";
+
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Context.Provider value={{
+          user: new UserStore()
+      }}>
+          <App />
+      </Context.Provider>
   </React.StrictMode>
 );
