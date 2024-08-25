706c2f0eba78384dc01c1bae05a14ae48e8ce1ee
Игорь <tawer0228@gmail.com>
2024-08-25 21:24:53 +1000

    add some content at product item page

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Card, Col, Image} from "react-bootstrap";
+import { Card, Col, Image } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { PRODUCT_ROUTE } from '../utils/consts';
 
-const ProductItem = ({product}) => {
+const ProductItem = ({ product }) => {
+    const navigate = useNavigate();
+
     return (
-        <Col md={5}>
-            <Card style={{width: 150, cursor: 'pointer'}} border={'light'}>
-                <Image width={350} height={350} src={product.img}/>
+        <Col md={5} className="mt-3" onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
+            <Card style={{ width: 150, cursor: 'pointer' }} border={'light'}>
+                <Image width={350} height={350} src={product.img} />
                 <div className="d-flex justify-content-center">
                     <div>Ardoni</div>
                 </div>
--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 
 const ProductPage = () => {
+    const product = {
+        id: 1,
+        name: 'Люфтен',
+        price: 15000,
+        img: 'https://thefurny.eu/upload/iblock/096/09675713a87bbdd4919393cd839a7457.jpg'
+    }
+    const description = [
+        {id:1, title: 'Каркас', description: 'Многослойная березовая фанера'},
+        {id:2, title: 'Наполнение', description: 'Пенополиуретан'},
+        {id:3, title: 'Наполнение подушек', description: 'Синтешар'},
+        {id:4, title: 'Жесткость', description: 'Средний'},
+    ]
+
     return (
-        <div>
-           ProductPage
-        </div>
+        <Container className={'mt-3'}>
+            <Row>
+                <Col md={4} className={'d-flex justify-content-center align-items-center'}>
+                    <Image width={350} height={350} src={product.img}/>
+                </Col>
+                <Col md={4} className={'d-flex justify-content-center align-items-center'}>
+                    <h2>{product.name}</h2>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className={'d-flex flex-column align-items-center justify-content-around'}
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От {product.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className={'d-flex flex-column m-2'}>
+                <h1>Характеристики</h1>
+                {description.map((info, index ) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
