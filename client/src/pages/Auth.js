754560e8356aedd578ec38bc2609a8ff2389faea
Игорь <tawer0228@gmail.com>
2024-08-25 11:49:46 +1000

    add uselocation for auth form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Button, Card, Container, Form, Row} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
-import {REGISTRATION_ROUTE} from "../utils/consts";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
         <Container
             className="d-flex justify-content-center align-items-center"
             style={{height: window.innerHeight - 54}}
         >
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">Авторизация</h2>
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control
                         className="mt-3"
@@ -20,16 +23,21 @@ const Auth = () => {
                         className="mt-3"
                         placeholder="Введите ваш пароль..."
                     />
-                    <Row className="d-flex justify-content-between pl-3 p-lg-3">
-                        <div>
-                            Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink>
-                        </div>
-                        <Button
-                            className="mt-3 align-self-end"
-                            variant={"outline-success"}
-                        >
-                            Войти
-                        </Button>
+                    <Row className="d-flex justify-content-between align-items-center mt-3 pl-3 pr-3">
+                        {isLogin ?
+                            <Col xs="auto">
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink>
+                            </Col>
+                            :
+                            <Col xs="auto">
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE }>Войдите!</NavLink>
+                            </Col>
+                        }
+                        <Col xs="auto">
+                            <Button variant="outline-success">
+                                {isLogin ? 'Войти' : 'Регистрация'}
+                            </Button>
+                        </Col>
                     </Row>
                 </Form>
             </Card>
