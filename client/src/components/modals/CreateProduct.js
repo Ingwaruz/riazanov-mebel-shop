424998ed655bf5a972f96219079b9a2286238b34
Игорь <tawer0228@gmail.com>
2024-08-27 21:02:29 +1000

    added the remaining functionality for full interaction with the server through the admin panel

--- client/src/components/modals/CreateProduct.js
+++ client/src/components/modals/CreateProduct.js
@@ -1,16 +1,45 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Col, Dropdown, Form, FormControl, Modal, Row} from "react-bootstrap";
 import {Context} from "../../index";
+import {createProduct, fetchFactories, fetchProducts, fetchTypes} from "../../http/productAPI";
+import {observer} from "mobx-react-lite";
 
-const CreateProduct = ({show, onHide}) => {
+const CreateProduct = observer(({show, onHide}) => {
     const {product} = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
     const [info, setInfo] = useState([])
+
+    useEffect(() => {
+        fetchTypes().then(data => product.setTypes(data))
+        fetchFactories().then(data => product.setFactories(data))
+    }, []);
 
     const addInfo = () => {
         setInfo([...info, {title: '', description: '', number: Date.now()}])
     }
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
+    }
+
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+    }
+
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    const addProduct = () => {
+        const formData = new FormData()
+        formData.append('name', name)
+        formData.append('price', `${price}`)
+        formData.append('img', file)
+        formData.append('factoryId', product.selectedFactory.id)
+        formData.append('typeId', product.selectedType.id)
+        formData.append('info', JSON.stringify(info))
+        createProduct(formData).then(data => onHide())
     }
 
     return (
@@ -28,26 +57,40 @@ const CreateProduct = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Dropdown className={'mt-2 mb-2'}>
-                        <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+                        <Dropdown.Toggle>{product.selectedType.name || 'Выберите тип'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {product.types.map(type =>
-                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                <Dropdown.Item
+                                    onClick={() => product.setSelectedType(type)}
+                                    key={type.id}
+                                >{
+                                    type.name}
+                                </Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Dropdown>
-                        <Dropdown.Toggle>Выберите производителя</Dropdown.Toggle>
+                        <Dropdown.Toggle>{product.selectedFactory.name || 'Выберите производителя'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {product.factories.map(factory =>
-                                <Dropdown.Item key={factory.id}>{factory.name}</Dropdown.Item>
+                                <Dropdown.Item
+                                    onClick={() => product.setSelectedFactory(factory)}
+                                    key={factory.id}
+                                >
+                                    {factory.name}
+                                </Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                         className={'mt-3'}
                         placeholder={'Введите название товара'}
                     />
                     <Form.Control
+                        value={price}
+                        onChange={e => setPrice(Number(e.target.value))}
                         className={'mt-3'}
                         placeholder={'Введите стоимость товара (От ... руб.)'}
                         type={'number'}
@@ -55,6 +98,7 @@ const CreateProduct = ({show, onHide}) => {
                     <Form.Control
                         className={'mt-3'}
                         type={'file'}
+                        onChange={selectFile}
                     />
                     <hr/>
                     <Button
@@ -67,11 +111,15 @@ const CreateProduct = ({show, onHide}) => {
                             <Row className='mt-4' key={i.number}>
                                 <Col md={4}>
                                     <Form.Control
+                                        value={i.title}
+                                        onChange={(e) => changeInfo('title', e.target.value, i.number)}
                                         placeholder={'Введите название характеристики'}
                                     />
                                 </Col>
                                 <Col md={4}>
                                     <Form.Control
+                                        value={i.description}
+                                        onChange={(e) => changeInfo('description', e.target.value, i.number)}
                                         placeholder={'Введите описание характеристики'}
                                     />
                                 </Col>
@@ -89,10 +137,10 @@ const CreateProduct = ({show, onHide}) => {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant={'outline-danger'} onClick={onHide}>Закрыть</Button>
-                <Button variant={'outline-success'} onClick={onHide}>Добавить</Button>
+                <Button variant={'outline-success'} onClick={addProduct}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     );
-};
+});
 
 export default CreateProduct;
