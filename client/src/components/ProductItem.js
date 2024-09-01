c86ad78179736850bc8f8784297ef734a004fbe0
Игорь <tawer0228@gmail.com>
2024-09-02 00:39:51 +1000

    minor product card ui changes

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Image } from 'react-bootstrap';
+import {Card, Col, Image, Row} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { PRODUCT_ROUTE } from '../utils/consts';
 
@@ -8,11 +8,15 @@ const ProductItem = ({ product }) => {
 
     return (
         <Col md={5} className="mt-5 d-flex justify-content-center " onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
-            <Card style={{ width: 150, cursor: 'pointer' }} border={'dark'}>
-                <Image width={350} height={350} src={process.env.REACT_APP_API_URL + product.img} />
-                <div className="d-flex justify-content-center">
-                    <div className={'d-flex justify-content-center align-items-center m-text'}>{product.name}</div>
-                </div>
+            <Card style={{ width: 368, cursor: 'pointer' }} border={'dark'} bg={'light'}>
+                <Image className={'m-lg-2'} width={350} height={350} src={process.env.REACT_APP_API_URL + product.img} />
+                <Col className={'d-flex justify-content-center align-items-center m-text'}>{product.name}</Col>
+                <Col className={'d-flex justify-content-center align-items-center xs-text m-lg-2'}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Col>
             </Card>
         </Col>
     );
