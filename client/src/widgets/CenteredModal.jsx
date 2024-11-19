49f439c4889e0b0143933f2886c2ec04517c3503
Игорь <tawer0228@gmail.com>
2024-11-19 15:05:01 +1000

    add modals for header

---
+++ client/src/widgets/CenteredModal.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import '../app/styles/colors.scss';
+
+const CenteredModal = ({ show, onHide, title, children }) => {
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title className='xl-text' style={{fontWeight : `600`}}>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body className='m-text'>
+                {children}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="secondary"
+                    onClick={onHide}
+                    className='m-text color-white'
+                >
+                    Закрыть
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default CenteredModal;
--- client/src/widgets/Navbar.jsx
+++ client/src/widgets/Navbar.jsx
@@ -1,95 +1,167 @@
-import React, { useContext } from 'react';
-import {Navbar as BootstrapNavbar, Container, Nav, Button, Row, Col, Image} from 'react-bootstrap';
+import React, { useState, useContext } from 'react';
+import { Navbar as BootstrapNavbar, Button, Row, Col, Image, Nav } from 'react-bootstrap';
 import { Context } from "../index";
-import {ADMIN_ROUTE, LOGIN_ROUTE, PRODUCT_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "../entities/utils/consts";
-import { NavLink } from "react-router-dom";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../entities/utils/consts";
 import { observer } from "mobx-react-lite";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, NavLink } from 'react-router-dom';
+import CenteredModal from './CenteredModal';
+import '../app/styles/colors.scss';
 
 const MyNavbar = observer(() => {
     const { user } = useContext(Context);
-    const navigate = useNavigate()
     const { product } = useContext(Context);
+    const navigate = useNavigate();
 
+    const [modalData, setModalData] = useState({ show: false, title: '', content: '' });
 
     const logOut = () => {
-        user.setUser({})
-        user.setIsAuth(false)
-        localStorage.removeItem('token')
+        user.setUser({});
+        user.setIsAuth(false);
+        localStorage.removeItem('token');
         navigate(LOGIN_ROUTE);
-    }
+    };
+
+    const handleModalShow = (title, content) => {
+        setModalData({ show: true, title, content });
+    };
+
+    const handleModalClose = () => {
+        setModalData({ ...modalData, show: false });
+    };
 
     return (
         <div>
-            <BootstrapNavbar data-bs-theme="dark" style={{height: 75, boxShadow: '0 4px 4px rgba(0, 0, 0, 0.4)'}} className={'s-text bg-color-gray'}>
+            <BootstrapNavbar
+                data-bs-theme="dark"
+                style={{ height: 75, boxShadow: '0 4px 4px rgba(0, 0, 0, 0.4)' }}
+                className={'s-text bg-color-gray'}
+            >
                 <div className={'container-fluid'}>
-                    <NavLink className={'s-text ps-3 color-white'} to={SHOP_ROUTE}>
+                    <NavLink
+                        className={'s-text ps-3 color-white'}
+                        to={SHOP_ROUTE}
+                        onClick={() => product.resetFilters()} // Фильтры сбрасываются при переходе
+                    >
                         <Image
                             className="icon"
                             width={50}
-                            onClick={() => { product.resetFilters() }}
                             src="/icons/riazanov-mebel.jpg"
+                            alt="Главная"
                         />
                     </NavLink>
+
                     <Row className={'d-flex justify-content-around color-white'} style={{ width: 600 }}>
                         <Col className={'xs-text mx-3 col-auto'}>
-                            <NavLink
-                                className={'color-white'}
-                                to={SHOP_ROUTE}
+                            <Button
+                                variant="link"
+                                className="color-white text-decoration-none"
+                                onClick={() =>
+                                    handleModalShow(
+                                        'Где купить',
+                                        <>
+                                            <p>
+                                                <span style={{fontWeight: '600'}} className="l-text">Благовещенск</span>
+                                                <br />
+                                                    1. ТРЦ Острова, улица Мухина, 144 <br />
+                                            </p>
+                                            <p>
+                                                <span style={{fontWeight: '600'}} className="l-text">Хабаровск</span>
+                                                <br/>
+                                                    1. Улица Волочаевская, 8д, 2 этаж<br />
+                                                    2. Проспект 60-летия Октября, 206, 3 этаж
+                                            </p>
+                                        </>
+                                    )
+                                }
                             >
                                 ГДЕ КУПИТЬ
-                            </NavLink>
+                            </Button>
                         </Col>
                         <Col className={'xs-text mx-3 col-auto'}>
-                            <NavLink
-                                className={'color-white'}
-                                to={SHOP_ROUTE}
+                            <Button
+                                variant="link"
+                                className="color-white text-decoration-none"
+                                onClick={() =>
+                                    handleModalShow(
+                                        'Распродажа',
+                                        'Информация о текущих скидках появится здесь.'
+                                    )
+                                }
                             >
                                 РАСПРОДАЖА
-                            </NavLink>
+                            </Button>
                         </Col>
                         <Col className={'xs-text mx-3 col-auto'}>
-                            <NavLink
-                                className={'color-white'}
-                                to={SHOP_ROUTE}
+                            <Button
+                                variant="link"
+                                className="color-white text-decoration-none"
+                                onClick={() =>
+                                    handleModalShow(
+                                        'Доставка',
+                                        'Информация о доставке появится здесь.'
+                                    )
+                                }
                             >
                                 ДОСТАВКА
-                            </NavLink>
+                            </Button>
                         </Col>
                         <Col className={'xs-text mx-3 col-auto'}>
-                            <NavLink
-                                className={'color-white'}
-                                to={SHOP_ROUTE}
+                            <Button
+                                variant="link"
+                                className="color-white text-decoration-none"
+                                onClick={() =>
+                                    handleModalShow(
+                                        'Контакты',
+                                        'Контактные номера:\n1. +7 (123) 456-78-90\n2. +7 (987) 654-32-10'
+                                    )
+                                }
                             >
                                 КОНТАКТЫ
-                            </NavLink>
+                            </Button>
                         </Col>
                     </Row>
 
-                    {user.isAuth ?
+                    {user.isAuth ? (
                         <Nav className="ml-auto color-white">
                             <Button
                                 className="ms-lg-4 s-text color-white border-radius-0 hover-item--white border-color-white bg-color-gray"
-                                onClick={() => navigate(ADMIN_ROUTE)}>
-                                Админ панель
+                                onClick={() => navigate(ADMIN_ROUTE)}
+                            >
+                                АДМИН ПАНЕЛЬ
                             </Button>
                             <Button
                                 className="ms-lg-4 s-text color-white border-radius-0 hover-item--white border-color-white bg-color-gray"
-                                onClick={() => logOut()}>
-                                Выйти
+                                onClick={logOut}
+                            >
+                                <Image
+                                    className="icon color-white"
+                                    width={30}
+                                    src="/icons/exit.svg"
+                                    alt="Главная"
+                                />
                             </Button>
                         </Nav>
-                        :
+                    ) : (
                         <Nav className="ml-auto">
                             <Button
                                 className={'border-radius-0 s-text hover-item--white color-white border-color-white bg-color-gray'}
-                                onClick={() => navigate(LOGIN_ROUTE)}>
+                                onClick={() => navigate(LOGIN_ROUTE)}
+                            >
                                 Авторизация
                             </Button>
                         </Nav>
-                    }
+                    )}
                 </div>
             </BootstrapNavbar>
+
+            {/* Модальное окно */}
+            <CenteredModal
+                show={modalData.show}
+                onHide={handleModalClose}
+                title={modalData.title}
+            >
+                <div style={{ whiteSpace: 'pre-line' }}>{modalData.content}</div>
+            </CenteredModal>
         </div>
     );
 });
