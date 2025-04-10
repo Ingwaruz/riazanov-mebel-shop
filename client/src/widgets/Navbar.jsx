cad861721fbfc9d200bb216b50c465605cf838ff
Игорь <tawer0228@gmail.com>
2025-04-10 13:05:49 +1000

    fix navbar

--- client/src/widgets/Navbar.jsx
+++ client/src/widgets/Navbar.jsx
@@ -37,132 +37,132 @@ const MyNavbar = observer(() => {
                 className={'s-text bg-main_color mb-4'}
             >
                 <div className={'container-fluid'}>
-                    <Col className="col-auto">
-                        <NavLink
-                            className={'s-text ps-3 color_white'}
-                            to={SHOP_ROUTE}
-                            onClick={() => product.resetFilters()} // Фильтры сбрасываются при переходе
-                        >
-                            <i className="fas fa-shop fa-2x"></i>
-                        </NavLink>
-                        <NavLink
-                            className="ms-5 s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
-                            to={SHOP_ROUTE}
-                            style={{border: 'none'}}
-                        >
-                            <i className="fas fa-user fa-2x"></i>
-                        </NavLink>
-                        <NavLink
-                            className="ms-5 s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
-                            to={SHOP_ROUTE}
-                            style={{border: 'none'}}
-                        >
-                            <i className="fas fa-cart-arrow-down fa-2x"></i>
-                        </NavLink>
-                    </Col>
-                    <Row
-                        className={'d-flex justify-content-around color_white flex-wrap'}
-                        style={{ width: '100%', maxWidth: 900 }}
-                    >
+                    <Row className="w-100 align-items-center flex-nowrap">
+                        {/* Левая часть с логотипом и иконками */}
+                        <Col xs="auto" className="d-flex align-items-center" style={{ minWidth: '200px' }}>
+                            <div className="d-flex justify-content-between w-100 ms-3">
+                                <NavLink
+                                    className={'s-text color_white'}
+                                    to={SHOP_ROUTE}
+                                    onClick={() => product.resetFilters()}
+                                >
+                                    <i className="fas fa-shop fa-2x"></i>
+                                </NavLink>
+                                <NavLink
+                                    className="s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
+                                    to={SHOP_ROUTE}
+                                    style={{border: 'none'}}
+                                >
+                                    <i className="fas fa-user fa-2x"></i>
+                                </NavLink>
+                                <NavLink
+                                    className="s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
+                                    to={SHOP_ROUTE}
+                                    style={{border: 'none'}}
+                                >
+                                    <i className="fas fa-cart-arrow-down fa-2x"></i>
+                                </NavLink>
+                            </div>
+                        </Col>
 
-                    <Col className={'xs-text mx-3 col-auto'}>
-                            <Button
-                                variant="link"
-                                className="color_white text-decoration-none m-text"
-                                onClick={() =>
-                                    handleModalShow(
-                                        'Где купить',
-                                        <>
-                                            <p>
-                                                <span style={{fontWeight: '600'}} className="l-text">Благовещенск</span>
-                                                <br />
+                        {/* Центральная часть с навигацией */}
+                        <Col className="d-flex justify-content-center px-4">
+                            <div className="d-flex justify-content-between w-100" style={{ maxWidth: '600px' }}>
+                                <Button
+                                    variant="link"
+                                    className="color_white text-decoration-none m-text d-none d-md-block p-0"
+                                    onClick={() =>
+                                        handleModalShow(
+                                            'Где купить',
+                                            <>
+                                                <p>
+                                                    <span style={{fontWeight: '600'}} className="l-text">Благовещенск</span>
+                                                    <br />
                                                     1. ТРЦ Острова, улица Мухина 144, 3 этаж <br />
-                                            </p>
-                                            <p>
-                                                <span style={{fontWeight: '600'}} className="l-text">Хабаровск</span>
-                                                <br/>
+                                                </p>
+                                                <p>
+                                                    <span style={{fontWeight: '600'}} className="l-text">Хабаровск</span>
+                                                    <br/>
                                                     1. Улица Волочаевская 8д, 2 этаж<br />
                                                     2. Проспект 60-летия Октября 206, 3 этаж
-                                            </p>
-                                        </>
-                                    )
-                                }
-                            >
-                                ГДЕ КУПИТЬ
-                            </Button>
+                                                </p>
+                                            </>
+                                        )
+                                    }
+                                >
+                                    ГДЕ КУПИТЬ
+                                </Button>
+                                <Button
+                                    variant="link"
+                                    className="color_white text-decoration-none m-text d-none d-lg-block p-0"
+                                    onClick={() =>
+                                        handleModalShow(
+                                            'Акции',
+                                            'Информация о текущих акциях появится здесь.'
+                                        )
+                                    }
+                                >
+                                    АКЦИИ
+                                </Button>
+                                <Button
+                                    variant="link"
+                                    className="color_white text-decoration-none m-text d-none d-lg-block p-0"
+                                    onClick={() =>
+                                        handleModalShow(
+                                            'Доставка',
+                                            'Информация о доставке появится здесь.'
+                                        )
+                                    }
+                                >
+                                    ДОСТАВКА
+                                </Button>
+                                <Button
+                                    variant="link"
+                                    className="color_white text-decoration-none m-text d-none d-md-block p-0"
+                                    onClick={() =>
+                                        handleModalShow(
+                                            'Контакты',
+                                            'Контактные номера:\n1. +7 (123) 456-78-90\n2. +7 (987) 654-32-10'
+                                        )
+                                    }
+                                >
+                                    КОНТАКТЫ
+                                </Button>
+                            </div>
                         </Col>
-                        <Col className={'xs-text mx-3 col-auto'}>
-                            <Button
-                                variant="link"
-                                className="color_white text-decoration-none m-text"
-                                onClick={() =>
-                                    handleModalShow(
-                                        'Акции',
-                                        'Информация о текущих акциях появится здесь.'
-                                    )
-                                }
-                            >
-                                АКЦИИ
-                            </Button>
-                        </Col>
-                        <Col className={'xs-text mx-3 col-auto'}>
-                            <Button
-                                variant="link"
-                                className="color_white text-decoration-none m-text"
-                                onClick={() =>
-                                    handleModalShow(
-                                        'Доставка',
-                                        'Информация о доставке появится здесь.'
-                                    )
-                                }
-                            >
-                                ДОСТАВКА
-                            </Button>
-                        </Col>
-                        <Col className={'xs-text mx-3 col-auto'}>
-                            <Button
-                                variant="link"
-                                className="color_white text-decoration-none m-text"
-                                onClick={() =>
-                                    handleModalShow(
-                                        'Контакты',
-                                        'Контактные номера:\n1. +7 (123) 456-78-90\n2. +7 (987) 654-32-10'
-                                    )
-                                }
-                            >
-                                КОНТАКТЫ
-                            </Button>
+
+                        {/* Правая часть с авторизацией */}
+                        <Col xs="auto" className="d-flex align-items-center ms-auto">
+                            {user.isAuth ? (
+                                <Nav className="d-flex align-items-center">
+                                    {user.user.role === 'ADMIN' && (
+                                        <Button
+                                            className="me-2 color_white border-radius-0 hover-item--white border-color_white bg-main_color m-text"
+                                            onClick={() => navigate(ADMIN_ROUTE)}
+                                        >
+                                            АДМИН ПАНЕЛЬ
+                                        </Button>
+                                    )}
+                                    <Button
+                                        className="s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
+                                        onClick={logOut}
+                                        style={{border: 'none'}}
+                                    >
+                                        <i className="fas fa-arrow-right-from-bracket fa-2x"></i>
+                                    </Button>
+                                </Nav>
+                            ) : (
+                                <Nav>
+                                    <Button
+                                        className={'border-radius-0 hover-item--white color_white border-color_white bg-main_color m-text'}
+                                        onClick={() => navigate(LOGIN_ROUTE)}
+                                    >
+                                        ВОЙТИ
+                                    </Button>
+                                </Nav>
+                            )}
                         </Col>
                     </Row>
-
-                    {user.isAuth ? (
-                        <Nav className="ml-auto color_white me-3">
-                            {user.user.role === 'ADMIN' && (
-                                <Button
-                                    className="ms-lg-4 color_white border-radius-0 hover-item--white border-color_white bg-main_color m-text"
-                                    onClick={() => navigate(ADMIN_ROUTE)}
-                                >
-                                    АДМИН ПАНЕЛЬ
-                                </Button>
-                            )}
-                            <Button
-                                className="ms-lg-4 s-text color_white border-radius-0 hover-item--white border-color_white bg-main_color"
-                                onClick={logOut}
-                                style={{border: 'none'}}
-                            >
-                                <i class="fas fa-arrow-right-from-bracket fa-2x"></i>
-                            </Button>
-                        </Nav>
-                    ) : (
-                        <Nav className="ml-auto">
-                            <Button
-                                className={'border-radius-0 hover-item--white color_white border-color_white bg-main_color m-text'}
-                                onClick={() => navigate(LOGIN_ROUTE)}
-                            >
-                                ВОЙТИ
-                            </Button>
-                        </Nav>
-                    )}
                 </div>
             </BootstrapNavbar>
 
